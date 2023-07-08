509f5b2a9bd21960b5a9f0740bb3e73a18e5a6ef
Nelson Mendez <105968684+nelsonmenza@users.noreply.github.com>
2023-07-08 16:08:43 -0700

    Fixed mistake in commit message: Added script to import variable and print its value

--- 0x02-Python-import_modules/modules/varModule.py
+++ 0x02-Python-import_modules/modules/varModule.py
@@ -4,8 +4,5 @@
 A script that imports a variable from another script and prints its value.
 """
 
-# Import the variable from the Varmodule.py script
-from varModule import string
-
-# Print the value of the imported variable
-print(string)
+# Define a variable named 'string' and assign it the value "Hola mundo!!!"
+string = "Hola mundo!!!"
