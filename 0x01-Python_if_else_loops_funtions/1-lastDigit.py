02b3a91fae651a2194e465245e10d09b4dee26c5
Nelson Mendez <105968684+nelsonmenza@users.noreply.github.com>
2023-07-15 11:15:52 -0700

    Added comments explain the purpose and functionality of each section of the code.

--- 0x01-Python_if_else_loops_funtions/1-lastDigit.py
+++ 0x01-Python_if_else_loops_funtions/1-lastDigit.py
@@ -1,13 +1,20 @@
 #!/usr/bin/python3
 import random
+
+# Generate a random number between -10000 and 10000.
 number = random.randint(-10000, 10000)
-# last_digit absolute number.
+
+# Calculate the last digit of the absolute number.
 last_digit = abs(number) % 10
-# if the number is less than 0 change to negative number.
+
+# If the number is less than 0, change the last digit to a negative number.
 if number < 0:
     last_digit = -abs(last_digit)
+
+# Print the last digit of the number.
 print("The last digit of {:d} is {:d}".format(number, last_digit), end="")
-#
+
+# Determine additional information based on the value of the number and last digit.
 if number > 5 and last_digit != 0:
     print(" and is greater than 5")
 elif number < 6 and last_digit != 0:
