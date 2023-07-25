8275291af59f6ff9b52e80f38f70d74ac9063cbf
Nelson Mendez <105968684+nelsonmenza@users.noreply.github.com>
2023-07-25 10:45:05 -0700

    Implemented command line argument summing with error handling.

--- 0x02-Python_import_modules/3-sumArgv.py
+++ 0x02-Python_import_modules/3-sumArgv.py
@@ -2,7 +2,14 @@
 # A script to sum all arguments
 import sys
 if __name__ == "__main__":
-
-    for arg in range(1, len(sys.argv)):
-        print(sys.argv[arg], end="")
-    
+    try:
+        if len(sys.argv) > 0:  # Check if there are command line arguments
+            sum_arg = 0  # Variable to store the sum of arguments
+            # Iterate through the command line arguments
+            for arg in range(1, len(sys.argv)):
+                # Convert the argument to an integer
+                arg_int = int(sys.argv[arg])
+                sum_arg += arg_int  # Add the integer argument to the sum
+        print(f"The result of summing the arguments is: {sum_arg}")
+    except ValueError:
+        print("Please enter correct characters that can be converted to integers.")
