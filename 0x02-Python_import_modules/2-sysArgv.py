51dd9aac38f5a8505b897eb26c314b049d3c89ec
Nelson Mendez <105968684+nelsonmenza@users.noreply.github.com>
2023-07-27 15:55:27 -0700

    Modified len(sys.argv) to use a variable for improved readability. Updated import statement for 'argv' from 'sys'.

--- 0x02-Python_import_modules/2-sysArgv.py
+++ 0x02-Python_import_modules/2-sysArgv.py
@@ -1,25 +1,20 @@
 #!/usr/bin/python3
-import sys
-
+from sys import argv
 if __name__ == "__main__":
+    arguments = len(argv)
     # Print the name of the program
     print("Command-Line Argument Analyzer")
-
-    # Print the name of the program (sys.argv[0])
-    print("This is the name of the program:", sys.argv[0])
-
-    # Print the total number of elements in sys.argv (program name + arguments)
-    print("Number of elements including the name of the program:", len(sys.argv))
-
+    # Print the total number of elements in argv (program name + arguments)
+    print("Number of Arguments including the name of the program:", arguments)
     # Print the number of elements excluding the name of the program (arguments only)
-    print("Number of elements excluding the name of the program:", len(sys.argv) - 1)
-
+    arg_count = arguments - 1
+    print("Number of arguments excluding the name of the program:", arg_count)
     # Check if there are additional arguments (excluding the program name)
-    if len(sys.argv) > 1:
+    if arg_count > 0:
         # If there are additional arguments, print them with their corresponding index
         print("Command-line arguments:")
-        for arg in range(1, len(sys.argv)):
-            print(f"{arg}: {sys.argv[arg]}")
+        for arg in range(1, arguments):
+            print(f"{arg}: {argv[arg]}")
     else:
         # If no additional arguments are provided, print a message indicating so
         print("No additional command-line arguments provided.")
--- 0x02-Python_import_modules/3-sumArgv.py
+++ 0x02-Python_import_modules/3-sumArgv.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
 # A script to sum all arguments
-import sys
+from sys import argv
 if __name__ == "__main__":
     try:
-        if len(sys.argv) > 0:  # Check if there are command line arguments
+        arguments = len(argv)
+        if arguments > 1:  # Check if there are command line arguments
             sum_arg = 0  # Variable to store the sum of arguments
             # Iterate through the command line arguments
-            for arg in range(1, len(sys.argv)):
+            for arg in range(1, arguments):
                 # Convert the argument to an integer
-                arg_int = int(sys.argv[arg])
+                arg_int = int(argv[arg])
                 sum_arg += arg_int  # Add the integer argument to the sum
-        print(f"The result of summing the arguments is: {sum_arg}")
-    except ValueError:
-        print("Please enter correct characters that can be converted to integers.")
+            print(f"The result of summing the arguments is: {sum_arg}")
+        else:
+            print("No command line arguments provided.")
+    except ValueError as e:
+        print(f"Invalid argument: {e}")
