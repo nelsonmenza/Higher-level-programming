614219b747a4ae9af509a58eb28714e38e425789
Nelson Mendez <105968684+nelsonmenza@users.noreply.github.com>
2023-07-28 10:29:45 -0700

    Modified position of argument for better understand.

--- 0x03-Python_data_structures/4-functionReplaceLst.py
+++ 0x03-Python_data_structures/4-functionReplaceLst.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 # Function to replace an element at a specific position
-def replace_element(lst, element, position):
+def replace_element(lst, position, new_element, ):
     # Check if position is valid
     if position >= len(lst):
         return "Enter a valid position."
@@ -10,12 +10,12 @@ def replace_element(lst, element, position):
         for i in lst:
             new_lst.append(i)
         # Replace the element at the specified position
-        new_lst[position] = element
+        new_lst[position] = new_element
         # Return the list with the new item
         return f'List with the element inserted: {new_lst}'
 
  # Initial list
 lst = [1, 2, 3, "a", 5, 6, 4]
 # Test the replace_element function
-print(replace_element(lst, 'NELSON', 6))
+print(replace_element(lst, 6, 'NELSON'))
 print(f'First list: {lst}')
