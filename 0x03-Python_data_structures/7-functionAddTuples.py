c19f0bcecf08572bd658c8513415fe693e1a8dc7
Nelson Mendez <105968684+nelsonmenza@users.noreply.github.com>
2023-07-29 09:39:36 -0700

    Improveved exercise

--- 0x03-Python_data_structures/7-functionAddTuples.py
+++ 0x03-Python_data_structures/7-functionAddTuples.py
@@ -1,18 +1,33 @@
 #!/usr/bin/python3
-# Function to add two tuples
+ # Function to add two tuples
 def add_tuples(tuple1, tuple2):
     new_tuple = []
     length1 = len(tuple1)
     length2 = len(tuple2)
-    num1 = 0
-    num2 = 0
-    max_length = max(length1, length2)
-    for i in range(max_length):
-        if i < length1:
-            num1 += tuple1[i]
-
-        if i < length2:
-            num2 += tuple2[i]
-
-    new_tuple.append(num1+num2)
-    return tuple(new_tuple)
+    i = 0
+     # Iterate through the tuples
+    while True:
+        sum = 0
+         # Check if both tuples have elements at the current index
+        if i < length1 and i < length2:
+            sum = tuple1[i] + tuple2[i]
+            i += 1
+            new_tuple.append(sum)
+         # If only tuple1 has elements at the current index
+        elif i < length1:
+            new_tuple.append(tuple1[i])
+            i += 1
+         # If only tuple2 has elements at the current index
+        elif i < length2:
+            new_tuple.append(tuple2[i])
+            i += 1
+         # If both tuples have been fully traversed
+        else:
+            break
+     return tuple(new_tuple)
+ tuple_a = (1, 89)
+ print(add_tuples(tuple_a, (1, 2, 3, 4, 5)))
+print(add_tuples(tuple_a, (1, 2, 3)))
+print(add_tuples(tuple_a, (1, 2)))
+print(add_tuples(tuple_a, (1,)))
+print(add_tuples(tuple_a, ()))
