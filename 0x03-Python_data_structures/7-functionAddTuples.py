b8ac84e59ec931f156d4e701514288b421699f80
Nelson Mendez <105968684+nelsonmenza@users.noreply.github.com>
2023-07-29 09:44:10 -0700

    Improveved indentations

--- 0x03-Python_data_structures/7-functionAddTuples.py
+++ 0x03-Python_data_structures/7-functionAddTuples.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
- # Function to add two tuples
+# Function to add two tuples
 def add_tuples(tuple1, tuple2):
     new_tuple = []
     length1 = len(tuple1)
     length2 = len(tuple2)
     i = 0
-     # Iterate through the tuples
+    # Iterate through the tuples
     while True:
         sum = 0
-         # Check if both tuples have elements at the current index
+        # Check if both tuples have elements at the current index
         if i < length1 and i < length2:
             sum = tuple1[i] + tuple2[i]
             i += 1
@@ -24,9 +24,12 @@ def add_tuples(tuple1, tuple2):
          # If both tuples have been fully traversed
         else:
             break
-     return tuple(new_tuple)
- tuple_a = (1, 89)
- print(add_tuples(tuple_a, (1, 2, 3, 4, 5)))
+    return tuple(new_tuple)
+
+
+# Test cases
+tuple_a = (1, 89)
+print(add_tuples(tuple_a, (1, 2, 3, 4, 5)))
 print(add_tuples(tuple_a, (1, 2, 3)))
 print(add_tuples(tuple_a, (1, 2)))
 print(add_tuples(tuple_a, (1,)))
